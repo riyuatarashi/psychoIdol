04e232a4e60614807c385d1284edf7cc7348b9b0
理由新し <moeetomoni@outlook.com>
2021-03-24 15:04:28 +0100

    feat(key-config): display menu & begin of menu manager
    création du gestionaire de menu pour simplifier les fonction et éviter
    la redondance

--- public/js/game.js
+++ public/js/game.js
@@ -1,6 +1,19 @@
 class Game {
 	constructor() {
-		
+		this.submits = document.querySelectorAll('.submit');
+
+		var _this = this;
+		window.addEventListener("keydown", function(e) {
+			if(e.key === "Escape") {
+				_this.manageMenu('key-menu');
+			}
+		});
+
+		for(let i=0; i < this.submits.length; i++) {
+			this.submits[i].addEventListener("click", function(e) {
+				_this.manageMenu(e.target.attributes.ref.value, "close");
+			});
+		}
 	}
 
 	update() {
@@ -8,5 +21,26 @@ class Game {
 
 		_OBJ_.camera.update();
 		_OBJ_.character.update(_OBJ_.camera.x, _OBJ_.camera.y);
+	}
+
+	manageMenu(menu, type = "open") {
+		switch(menu) {
+			case 'key-menu':
+				if(type == "open") {
+					this.diplayMenu(menu);
+				}
+				else {
+					this.hideMenu(menu);
+				}
+				break;
+		}
+	}
+
+	diplayMenu(menu) {
+		document.getElementById(menu).classList.remove('d-none');
+	}
+
+	hideMenu(menu) {
+		document.getElementById(menu).classList.add('d-none');
 	}
 }
